f2124c59d0ddfdaab0ae8516cf1f13869fe46629
KristianWold <kriswold@student.matnat.uio.no>
2021-04-12 18:21:48 +0200

    Run multiple instances of regularized models

--- src/samplers.py
+++ src/samplers.py
@@ -22,3 +22,30 @@ class Parity():
         output = output / shots
 
         return output
+
+
+class AverageBit():
+    def __call__(self, counts):
+        shots = sum(counts.values())
+        output = 0
+        for bitstring, samples in counts.items():
+            bits = [int(bit) for bit in bitstring]
+            average = sum(bits) / len(bits)
+            output += average * samples
+
+        output = output / shots
+
+        return output
+
+
+class LastBit():
+    def __call__(self, counts):
+        shots = sum(counts.values())
+        output = 0
+        for bitstring, samples in counts.items():
+            if bitstring[-1] == "1":
+                output += samples
+
+        output = output / shots
+
+        return output
